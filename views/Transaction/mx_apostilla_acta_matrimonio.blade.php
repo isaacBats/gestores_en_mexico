628986dd5fc354d976f1aed3586e6e5d74143b08
AtaqueVisual <ataquevisual@gmail.com>
2017-04-17 11:24:14 -0500

    Se agregó un párrafo en el contenido

--- views/Transaction/mx_apostilla_acta_matrimonio.blade.php
+++ views/Transaction/mx_apostilla_acta_matrimonio.blade.php
@@ -8,6 +8,7 @@
                     <h1 class="titulo mayus">Apostilla de acta de matrimonio</h1>
                     <p>¿Va a utilizar su acta en otro país? le será necesario el sello del convenio de la halla, evite perder tiempo, hágalo rápido y a la primera, sin largas filas. Nosotros lo hacemos por usted. ¿No la tiene aún? Tramítala aquí.</p>
                     <p><em>En caso de que tu trámite requiera envío de documentos originales a nuestras oficinas, te notificaremos por correo electrónico.</em></p>
+                    <p><strong>Para este trámite nos es necesario contar con el original de su acta a apostillar de reciente expedición, es necesario nos la haga llegar, si no la tiene aún despreocúpese, la tramitamos por usted, déjenos saberlo en el espacio de comentarios.</strong></p>
                     <h4 class="mayus light azul noneo">Requisitos</h4>
                     <ul class="noneo">
                         <li>Requisito 1</li>
