56693f26209a942e1d2bbd415b199d15b7613626
Isaac Daniel Batista <disa.indus@gmail.com>
2017-12-13 23:50:30 -0600

    Comments on Registry and license

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -1,5 +1,12 @@
 <?php 
-
+/*
+ * This file is part of AtaqueVisual.
+ *
+ * (c) Isaac Daniel Batista <@codeisaac>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Olive\controllers;
 	
--- src/controllers/CountryController.php
+++ src/controllers/CountryController.php
@@ -2,6 +2,14 @@
 
 use Olive\controllers\Controller;
 use Olive\infrastructure\ContryRepo;
+/*
+ * This file is part of AtaqueVisual.
+ *
+ * (c) Isaac Daniel Batista <@codeisaac>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 /**
 * Country Controller
