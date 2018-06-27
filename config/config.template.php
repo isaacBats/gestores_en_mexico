1483a0efd950a29f987bbc5cd2b261af8ecbb55a
Isaac Daniel Batista <disa.indus@gmail.com>
2018-06-26 23:48:11 -0500

    update config/config.template.php

--- config/config.template.php
+++ config/config.template.php
@@ -1,12 +1,19 @@
 <?php 
-	// // TRACER
-	$whoops = new \Whoops\Run;
-	$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-	$whoops->register();
+	
+	include __DIR__ . "/mail.config.php";
 
 	//  ROOT DIRECTORY
 	define('__OLIVE__',  __DIR__."/.." );
 	define('__ASSETS__',  __DIR__."/../assets/" );
+	define('ENVIROMENT', 'dev');
+	
+	if (ENVIROMENT != 'prod') {
+		// // TRACER
+		$whoops = new \Whoops\Run;
+		$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+		$whoops->register();
+	}
+
 
 	$cfg = new \Spot\Config();
 	$cfg->addConnection('mysql', [
