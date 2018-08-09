89ab6c152ccd23358aa66906ad1fbe5550febeb0
Isaac Daniel Batista <disa.indus@gmail.com>
2018-08-09 01:22:23 -0500

    Se crea el formulario de traducciones certificadas #5

---
+++ views/Transaction/mx_traducciones_certificadas.blade.php
@@ -0,0 +1,8 @@
+<div class="col-md-12">
+    <h6>Adjunta copia de tu documento (archivos .jpg con peso máximo de 1Mb)</h6>
+</div>
+<div class="col-md-12">
+    <div class="form-group">
+        <input name="attr_image" id="file-1" type="file" class="file" data-preview-file-type="any">
+    </div>
+</div>
