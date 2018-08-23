acc2b55908c1d327cd04ae594484abec6e0d58ea
Isaac Daniel Batista <disa.indus@gmail.com>
2018-08-23 00:43:19 -0500

    Add field state, because selecting the state the price is added

--- views/Transaction/mx_apostilla_usa.blade.php
+++ views/Transaction/mx_apostilla_usa.blade.php
@@ -5,6 +5,14 @@
         @endforeach
     </select>
 </div>
+<div class="col-md-4">
+    <select name="attr_estado" id="attr_estado" class="form-control light">
+        <option value="">Estado ubicación actual</option>
+        @foreach ($states as $state)
+            <option value="{{ $state->id }}">{{ utf8_encode($state->name) }}</option>
+        @endforeach
+    </select>
+</div>
 <div class="col-md-12">
     <h6>Adjunta copia de tu documento (archivos .jpg con peso máximo de 1Mb)</h6>
 </div>
